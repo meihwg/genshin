67edb14540fc0895dc5583d54fb21c4ff21d96f0
meihwg <belgrandlaureen.tg2@gmail.com>
2024-09-24 11:21:50 +0200

    first commit

---
+++ src/components/main-page/main-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main-page.css';
+
+const MainPage: React.FC = () => {
+    return (
+        <>
+            <h1 className="main-page">Main Page</h1>
+        </>
+    );
+}
+
+export default MainPage;
