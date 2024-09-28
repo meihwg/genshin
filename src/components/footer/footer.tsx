99bb1ccdc2e4cb68954bd3df0632fa4bb36acf5d
meihwg <belgrandlaureen.tg2@gmail.com>
2024-09-28 13:34:29 +0200

    update footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,15 +8,17 @@ const Footer: React.FC = () => {
                 <div className="footer-container">
                     <div className="footer-content">
                         <div className="col">
-                            <h3>Developed by Mei</h3>
+                            <h3>About me</h3>
                             <p>[icone] Github</p>
                             <p>[icone] Twitter</p>
                             <p>[icone] Discord</p>
                             <p>[icone] Mail : blabla</p>
                         </div>
                         <div className="col">
-                            <h3>Footer</h3>
-                            <p>Footer content</p>
+                            <h3>Developed by</h3>
+                            <p>Mei</p>
+                            <h4>With the help of</h4>
+                            <p>Erlow</p>
                         </div>
                         <div className="col">
                             <p>Genshin Impact™ is a registered trademark of hoYoverse Co., Ltd. This site is not affiliated with or endorsed by hoYoverse. Images and data © hoYoverse Co., Ltd.</p>
