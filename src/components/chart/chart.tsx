55e7adbbb65fa505005481e2d4db39ea796eee43
meihwg <belgrandlaureen.tg2@gmail.com>
2024-09-28 22:02:59 +0200

    ajout des charts pour les stats de l'utilisateur

---
+++ src/components/chart/chart.tsx
@@ -0,0 +1,69 @@
+
+import React, { useState, useEffect } from 'react';
+import { Chart } from 'primereact/chart';
+
+import './chart.css';
+
+interface DoughnutChartProps {
+    value: number;
+    color: string;
+}
+
+const DoughnutChart: React.FC<DoughnutChartProps> = ({ value, color }) => {
+    
+    const [chartData, setChartData] = useState({});
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const data = {
+            labels: ['', ''],
+            datasets: [
+                {
+                    data: [value, 100 - value],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue(color),
+                        documentStyle.getPropertyValue('--color-bg-light')
+                    ],
+                    hoverBackgroundColor: [
+                        documentStyle.getPropertyValue(color),
+                        documentStyle.getPropertyValue('--color-bg-light')
+                    ]
+                }
+            ]
+        };
+        const options = {
+            cutout: '60%',
+            aspectRatio: 0,
+            // texte au centre du donut
+            plugins: {
+                tooltip: {
+                    enabled: false,
+                },
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: value + '%',
+                    font: {
+                        size: 20
+                    }
+                },
+            }
+
+        };
+
+        setChartData(data);
+        setChartOptions(options);
+    }, []);
+
+    return (
+        <div className="chart-container">
+            <Chart type="doughnut" data={chartData} options={chartOptions} className="chart-container" />
+        </div>
+    )
+}
+
+export default DoughnutChart;
+        
--- src/components/user-stats/user-stats.tsx
+++ src/components/user-stats/user-stats.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import "./user-stats.css";
 
+import Chart from "../chart/chart.tsx";
+
 const UserStats: React.FC = () => {
     return (
         <>
             <div className="user-stats">
                 <div className="stats-container">
                     <div className="stat-explo stats">
-                        <h3>Exploration</h3>
-                        <p>60%</p>
+                        <h3 className="text-red">Exploration</h3>
+                        <div className="chart-container">
+                            <Chart value={60} color="--color-red" />
+                        </div>
                         <p>30 areas finished / 60</p>
                     </div>
                     <div className="stat-quest stats">
-                        <h3>Quests</h3>
-                        <p>40%</p>
+                        <h3 className="text-blue">Quests</h3>
+                        <div className="chart-container">
+                            <Chart value={40} color="--color-blue" />
+                        </div>
                         <p>20 quests finished / 50</p>
                     </div>
                     <div className="stat-success stats">
-                        <h3>Success</h3>
-                        <p>80%</p>
+                        <h3 className="text-yellow">Success</h3>
+                        <div className="chart-container">
+                            <Chart value={80} color="--color-yellow" />
+                        </div>
                         <p>1200 success / 1500</p>
                     </div>
                 </div>
