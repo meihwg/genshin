9b0872944065417c2384babaed981b474aec9c5e
meihwg <belgrandlaureen.tg2@gmail.com>
2024-09-29 14:48:38 +0200

    arrondi stats

--- src/components/user-stats/user-stats.tsx
+++ src/components/user-stats/user-stats.tsx
@@ -11,23 +11,23 @@ const UserStats: React.FC = () => {
                     <div className="stat-explo stats">
                         <h3 className="text-red">Exploration</h3>
                         <div className="chart-container">
-                            <Chart value={60} color="--color-red" />
+                            <Chart value={Math.round(30/60*100 * Math.pow(10, 2)) / Math.pow(10, 2)} color="--color-red" />
                         </div>
                         <p>30 areas finished / 60</p>
                     </div>
                     <div className="stat-quest stats">
                         <h3 className="text-blue">Quests</h3>
                         <div className="chart-container">
-                            <Chart value={40} color="--color-blue" />
+                            <Chart value={Math.round(20/50*100 * Math.pow(10, 2)) / Math.pow(10, 2)} color="--color-blue" />
                         </div>
                         <p>20 quests finished / 50</p>
                     </div>
                     <div className="stat-success stats">
                         <h3 className="text-yellow">Success</h3>
                         <div className="chart-container">
-                            <Chart value={80} color="--color-yellow" />
+                            <Chart value={Math.round(1246/1500*100 * Math.pow(10, 2)) / Math.pow(10, 2)} color="--color-yellow" />
                         </div>
-                        <p>1200 success / 1500</p>
+                        <p>1246 success / 1500</p>
                     </div>
                 </div>
             </div>
